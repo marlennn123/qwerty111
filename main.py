d80422b3cced9e7c1b558cab37d9bb1d7244eab2
marlennn123 <mkanatbekov777@gmail.com>
2024-11-10 11:17:59 +0600

    fsdfsaf

--- main.py
+++ main.py
@@ -14,3 +14,10 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+fdgkfdkgjhfdsgd
+gdkjfgjksdfgfd
+gsdfkgjhdfsgdsg
+dsgdsfghjfdghjsdfg
+dfgkjhdsgdsfg
+dsfgdjsjfghdfkjgds
